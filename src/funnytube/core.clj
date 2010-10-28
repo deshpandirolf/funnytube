c79b047e5051e2b042b1c68fb350f93de5e36358
Rohan Deshpande <rohan.deshpande@gmail.com>
2010-10-28 16:42:52 -0400

    add upvote downvote

--- src/funnytube/core.clj
+++ src/funnytube/core.clj
@@ -10,12 +10,20 @@
 
 (mongo! :db "funnytube")
 
+(defn update-score [id delta]
+  (let [video (fetch-one :videos :where {:_id id})]
+    (update! :videos video (merge video { :score (+ delta (:score video)) })))
+  (response/redirect "/"))
+
+(defn upvote [id] (update-score id 1))
+(defn downvote [id] (update-score id -1))
+
 (defn id-for-url [url]
   (nth (or (re-find #"youtube\.com.*\?v=(\w+)" url)
            (re-find #"youtube\.com/v/(\w+)" url)) 1))
 
 (defn submit [url]
-  (insert! :videos {:_id (id-for-url url)})
+  (insert! :videos {:_id (id-for-url url) :score 0})
   (response/redirect "/"))
 
 (defn render-video [key]
@@ -52,6 +60,8 @@
 
 (defroutes main-routes
   (GET "/" [] index)
+  (POST "/downvote" {params :params} (upvote (params "id")))
+  (POST "/upvote" {params :params} (upvote (params "id")))
   (POST "/submit" {params :params} (submit (params "v")))
   (route/files "/static" {:root "static"})
   (route/not-found "not found"))
